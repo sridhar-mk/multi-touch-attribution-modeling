30582c836252c400a245eab9416212025b28b670
sridhar-mk <145108737+sridhar-mk@users.noreply.github.com>
2026-06-05 10:09:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ st.set_page_config(
 
 st.title("Multi-Touch Attribution Modelling Dashboard")
 
-df = pd.read_csv(
-    "ga_sessions.csv",
-    low_memory=False
-)
+import os
+
+file_path = os.path.join("data", "ga_sessions.csv")
+df = pd.read_csv(file_path, low_memory=False)
 if uploaded_file:
 
     df = pd.read_csv(
